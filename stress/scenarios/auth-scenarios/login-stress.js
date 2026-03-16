2a9e402be90ba0de3b0c5c882a403701c0654553
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-16 02:06:21 +0200

    Refactor comment in thresholds section of login stress test for clarity

--- stress/scenarios/auth-scenarios/login-stress.js
+++ stress/scenarios/auth-scenarios/login-stress.js
@@ -9,7 +9,7 @@ export const options = {
         { duration: '2m', target: 0} // ramp down to 0 users over 2 minutes
     ],
     thresholds: {
-        http_req_duration: ['p(95)<500'], //
+        http_req_duration: ['p(95)<500'], 
         http_req_failed: ['rate<0.01'],
     },
 };
