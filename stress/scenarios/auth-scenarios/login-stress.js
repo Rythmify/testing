b59ccd4171f77565fec60373cf7a9c1912efd2c9
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-19 05:20:35 +0200

    Refactor user load stages in login and refresh stress test scenarios for accuracy

--- stress/scenarios/auth-scenarios/login-stress.js
+++ stress/scenarios/auth-scenarios/login-stress.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 200}, // ramp up to 200 users over 1 minute
-        { duration: '3m', target: 500}, // stay at 500 users for 3 minutes
-        { duration: '1m', target: 1000}, // ramp up to 1000 users over 1 minute
+        { duration: '1m', target: 10}, // ramp up to 200 users over 1 minute
+        { duration: '2m', target: 20}, // stay at 500 users for 3 minutes
+        { duration: '1m', target: 50}, // ramp up to 1000 users over 1 minute
         { duration: '2m', target: 0} // ramp down to 0 users over 2 minutes
     ],
     thresholds: {
@@ -16,14 +16,15 @@ export const options = {
 
 export default function () {
     const payload = JSON.stringify({
-        email: 'test@test.com',
-        password: 'test1234',
+        identifier: 'listener4@example.com', password: 'Listener1234!'
     });
     const response = http.post('http://localhost:8080/api/v1/auth/login', payload, {
         headers: {
             'Content-Type': 'application/json',
         }
     });
+    console.log('login status: ' + response.status);
+    console.log('login body: ' + response.body);
     check(response, {
         'status is 200':  (r) => r.status === 200,
         'has access token': (r) => JSON.parse(r.body).data?.access_token !== undefined,
--- stress/scenarios/auth-scenarios/refresh-stress.js
+++ stress/scenarios/auth-scenarios/refresh-stress.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 200}, // ramp up to 200 users over 1 minute
-        { duration: '3m', target: 500}, // stay at 500 users for 3 minutes
-        { duration: '1m', target: 1000}, // ramp up to 1000 users over 1 minute
+        { duration: '1m', target: 100}, // ramp up to 200 users over 1 minute
+        { duration: '3m', target: 200}, // stay at 500 users for 3 minutes
+        { duration: '1m', target: 300}, // ramp up to 1000 users over 1 minute
         { duration: '2m', target: 0} // ramp down to 0 users over 2 minutes
     ],
     thresholds: {
@@ -15,12 +15,17 @@ export const options = {
 };
 
 export default function () {
+    http.post('http://localhost:8080/api/v1/auth/login',
+    JSON.stringify({ identifier: 'listener4@example.com', password: 'Listener1234!' }),
+    { headers: { 'Content-Type': 'application/json' } }
+    );
     const response = http.post('http://localhost:8080/api/v1/auth/refresh',null, {
         headers: {
             'Content-Type': 'application/json',
         },
     });
-
+    console.log('refresh status: ' + response.status);
+    console.log('refresh body: ' + response.body);
     check(response, {
         'status is 200':(r) => r.status === 200,
         'has new access_token': (r) => JSON.parse(r.body).data?.access_token !== undefined,
