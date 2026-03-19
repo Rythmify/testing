6ea0376517f3103eda4284bd85c8f31a2f4094be
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-19 06:58:06 +0200

    Adjust user stress stages in registration stress test for increased concurrency

--- stress/scenarios/auth-scenarios/register-stress.js
+++ stress/scenarios/auth-scenarios/register-stress.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export const options = {
     stages: [
-        { duration: '1m', target: 10}, // ramp up to 10 users over 1 minute
-        { duration: '3m', target: 15}, // stay at 15 users for 3 minutes
-        { duration: '1m', target: 30}, // ramp up to 30 users over 1 minute
+        { duration: '1m', target: 50}, // ramp up to 50 users over 1 minute
+        { duration: '3m', target: 100}, // stay at 100 users for 3 minutes
+        { duration: '1m', target: 150}, // ramp up to 150 users over 1 minute
         { duration: '2m', target: 0} // ramp down to 0 users over 2 minutes
     ],
     thresholds: {
