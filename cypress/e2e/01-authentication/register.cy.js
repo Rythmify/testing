b73df29d972b04f028eb1e52b9a722a03a4fc908
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-21 00:48:36 +0200

    Update email in registration tests for consistency

--- cypress/e2e/01-authentication/register.cy.js
+++ cypress/e2e/01-authentication/register.cy.js
@@ -2,12 +2,12 @@ import {SignUpSelectors} from '../../support/selectors/auth.selectors'
 describe('Register Page', () => {
     beforeEach(() => {
         cy.visit('/signin');
-        cy.get(SignUpSelectors.emailInput).type('listener11@example.com');
+        cy.get(SignUpSelectors.emailInput).type('listener12@example.com');
         cy.get(SignUpSelectors.continueButton).click();
         cy.get(SignUpSelectors.createAccountButton).click();
     })
     it('Should display Create an account ',  () => {
-        cy.contains(/listener11@example.com/i).should('be.visible');
+        cy.contains(/listener12@example.com/i).should('be.visible');
         cy.contains(/Create an account/i).should('be.visible');
     })
     // Happy path test case for register with email and password
