ae9c2e88274dfa8a27c0ef1c83dc4966eba0cdce
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-21 13:18:35 +0200

    Enhance authentication tests by adding click actions on email input fields for better interaction flow

--- cypress/e2e/01-authentication/login.cy.js
+++ cypress/e2e/01-authentication/login.cy.js
@@ -20,6 +20,7 @@ describe('Login Page', () => {
     })
     // Happy path test case for login with email and password
     it ('Should login with email and password successfully and redirect to discover page' , () => {
+        cy.get(LoginSelectors.emailInput).click();
         cy.get(LoginSelectors.emailInput).type('listener4@example.com');
         cy.get(LoginSelectors.continueButton).click();
         cy.get(LoginSelectors.passwordInput).type('Listener1234!');
@@ -36,12 +37,14 @@ describe('Login Page', () => {
         cy.contains(/Please enter your email address./i).should('be.visible');
     })
     it('Should show error message for invalid email format', () => {
+        cy.get(LoginSelectors.emailInput).click();
         cy.get(LoginSelectors.emailInput).type('listener4example.com');
         cy.contains(/Enter a valid email address./i).should('not.exist');
         cy.get(LoginSelectors.continueButton).click();
         cy.contains(/Enter a valid email address./i).should('be.visible');
     })
     it('Should show error message for not add password', () => {
+        cy.get(LoginSelectors.emailInput).click();
         cy.get(LoginSelectors.emailInput).type('listener4@example.com');
         cy.get(LoginSelectors.continueButton).click();
         cy.contains(/Please enter your password./i).should('not.exist');
@@ -49,6 +52,7 @@ describe('Login Page', () => {
         cy.contains(/Please enter your password./i).should('be.visible');
     })
     it('Should show error message for short password', () => {
+        cy.get(LoginSelectors.emailInput).click();
         cy.get(LoginSelectors.emailInput).type('listener4@example.com');
         cy.get(LoginSelectors.continueButton).click();
         cy.contains(/Password must be at least 8 characters./i).should('not.exist');
