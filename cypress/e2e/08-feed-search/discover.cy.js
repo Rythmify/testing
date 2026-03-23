6232657a870959eafb9c2293d5e1ac18c682c34c
Ahmed Attay <ahmedattay8@gmail.com>
2026-03-23 14:15:16 +0200

    Add tests for track navigation and playback on Discover page

--- cypress/e2e/08-feed-search/discover.cy.js
+++ cypress/e2e/08-feed-search/discover.cy.js
@@ -12,5 +12,31 @@ describe ('Discover Page', () => {
         cy.get(DiscoverSelectors.artistToolsCollapseButton).click();
         cy.get(DiscoverSelectors.artistToolAmplifyButton).click().location("pathname",/checkout/i);
     })
-    
+    it('Should navigate to track page when track card is clicked',() => {
+        cy.get(DiscoverSelectors.trackCard).first().click();
+        cy.location("pathname",/\/[^\/]+\/[^\/]+/i);
+    })
+    it('Should display track cards on discover page', () => {
+        cy.get(DiscoverSelectors.trackCard)
+        .should('have.length.greaterThan', 0)
+    })
+    it ('Should play a track when play button is clicked', () => {
+        cy.get(DiscoverSelectors.trackCard).first().trigger('mouseover');
+        cy.get(DiscoverSelectors.playButton).first().click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+    })
+    // Corner case
+    it('Should expand artist tools after collapsing', () => {
+        cy.get(DiscoverSelectors.artistToolsCollapseButton).click()
+        cy.get(DiscoverSelectors.artistToolsCollapseButton).click()
+        cy.get(DiscoverSelectors.artistToolMonetizeButton)
+        .should('be.visible')
+    })
+
+    it('Should display carousel with multiple tracks', () => {
+        cy.get(DiscoverSelectors.carouselScrollContainer)
+        .should('be.visible')
+        .children()
+        .should('have.length.greaterThan', 1)
+    })
 })
--- cypress/support/selectors/feed-search.selectors.js
+++ cypress/support/selectors/feed-search.selectors.js
@@ -6,4 +6,9 @@ export const DiscoverSelectors = {
     artistToolsCtaButton: '[data-test="button-artist-tools-cta"]',
     artistListSectionRefresh: '[data-test="artist-list-section-refresh"]',
     artistListSection: '[data-test="artist-list-section"]',
+    trackCard: '[data-test="card-track"]',
+    playButton : '[data-test="button-play"]',
+    carouselScrollContainer: '[data-test="carousel-scroll-container"]',
+    trendingContainer: '[data-test="trending-container"]',
+    stickyPlayer: '[data-test="sticky-player"]',
 }
