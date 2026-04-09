91da6b771b72fb54af76960e62951ec870d9ac34
Ahmed Attay <ahmedattay8@gmail.com>
2026-04-10 01:23:20 +0200

    Add new tests for following, liking tracks and playlists, and sticky player visibility

--- cypress/e2e/07-playlists/library.cy.js
+++ cypress/e2e/07-playlists/library.cy.js
@@ -54,4 +54,78 @@ describe ('Library Page', () => {
         cy.reload();
         cy.get(PlaylistsSelectors.libraryLikesCards).should('have.length.greaterThan',0);
     })
+    it('should follow from sticky bar and show in following section', () => {
+        cy.visit('/discover');
+        cy.get(DiscoverSelectors.trackCard).first().trigger('mouseover');
+        cy.get(DiscoverSelectors.playButton).first().click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+
+        cy.get(PlaylistsSelectors.playerArtistName).invoke('attr', 'href').then((href) => {
+            const artistUserName = (href || '').split('/').filter(Boolean)[0];
+
+            cy.get(PlaylistsSelectors.playerFollowButton).click();
+
+            cy.visit('/you/library');
+            cy.get(PlaylistsSelectors.libraryFollowing).should('be.visible');
+            cy.get(PlaylistsSelectors.libraryFollowingCards).should('contain', artistUserName);
+        });
+    })
+    it('should like from sticky bar and show in likes section', () => {
+        cy.visit('/discover');
+        cy.get(DiscoverSelectors.trackCard).first().trigger('mouseover');
+        cy.get(DiscoverSelectors.playButton).first().click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+
+        cy.get(PlaylistsSelectors.playerTrackTitle).invoke('text').then((trackTitle) => {
+            cy.get(PlaylistsSelectors.playerLikeButton).click();
+
+            cy.visit('/you/library');
+            cy.get(PlaylistsSelectors.libraryLikes).should('be.visible');
+            cy.get(PlaylistsSelectors.libraryLikes).should('contain', trackTitle.trim());
+        });
+    })
+    it('should like playlist from discover and remove it after unlike', () => {
+        cy.visit('/discover');
+        cy.get(PlaylistsSelectors.playlistCard).first().find('p').first().invoke('text').then((playlistTitle) => {
+            const selectedPlaylistTitle = playlistTitle.trim();
+
+            cy.get(PlaylistsSelectors.playlistCard).first().trigger('mouseover');
+            cy.get(PlaylistsSelectors.playlistCard).first().find(PlaylistsSelectors.likeButton).first().click();
+
+            cy.visit('/you/library');
+            cy.get(PlaylistsSelectors.playlistFilterToggle).click();
+            cy.get(PlaylistsSelectors.playlistFilterLikedOption).click();
+            cy.get(PlaylistsSelectors.libraryPlaylists).should('be.visible');
+            cy.get(PlaylistsSelectors.libraryPlaylistsCards).should('contain', selectedPlaylistTitle);
+
+            cy.get(PlaylistsSelectors.libraryPlaylistsCards).contains(selectedPlaylistTitle).closest(PlaylistsSelectors.playlistCard).trigger('mouseover').find(PlaylistsSelectors.likeButton).first().click();
+            cy.get(PlaylistsSelectors.libraryPlaylistsCards).should('not.contain', selectedPlaylistTitle);
+        });
+    })
+    it('should keep sticky player visible when moving from discover to library', () => {
+        cy.visit('/discover');
+        cy.get(DiscoverSelectors.trackCard).first().trigger('mouseover');
+        cy.get(DiscoverSelectors.playButton).first().click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+        cy.contains('Library').click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+    })
+    it('should keep sticky player visible after reload when a track is playing', () => {
+        cy.visit('/discover');
+        cy.get(DiscoverSelectors.trackCard).first().trigger('mouseover');
+        cy.get(DiscoverSelectors.playButton).first().click();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+        cy.reload();
+        cy.get(DiscoverSelectors.stickyPlayer).should('be.visible');
+    })
+    it('should switch playlists filter between liked and all', () => {
+        cy.get(PlaylistsSelectors.playlistFilterToggle).click();
+        cy.get(PlaylistsSelectors.playlistFilterLikedOption).click();
+        cy.get(PlaylistsSelectors.libraryPlaylists).should('be.visible');
+        cy.get(PlaylistsSelectors.libraryPlaylistsCards).should('be.visible');
+        cy.get(PlaylistsSelectors.playlistFilterToggle).click();
+        cy.contains('All').click();
+        cy.get(PlaylistsSelectors.libraryPlaylists).should('be.visible');
+        cy.get(PlaylistsSelectors.libraryPlaylistsCards).should('be.visible');
+    })
 })
