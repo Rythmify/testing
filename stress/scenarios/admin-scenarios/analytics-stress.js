7dc4b1f185939b22112fca3975d751cd83db1db2
yoeweida36 <yoeweida@gmail.com>
2026-04-25 10:46:24 +0300

    update analutics by deployed URL & response messages

--- stress/scenarios/admin-scenarios/analytics-stress.js
+++ stress/scenarios/admin-scenarios/analytics-stress.js
@@ -1,7 +1,7 @@
 // ! Backend API not fully implemented, so this test is expected to fail until the analytics endpoint is ready.
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-export const BASE_URL = 'http://localhost:8080/api/v1'; 
+export const BASE_URL = 'https://rythmify-backend-dev.livelypebble-6b7965ef.uaenorth.azurecontainerapps.io/api/v1'; 
 
 export const options = {
     stages: [
@@ -54,13 +54,15 @@ export default function () {
     console.log('analytics body: ' + response.body);
 
     check(response, {
-        'status is 200':       (r) => r.status === 200,
-        'has data':            (r) => {
-        try { return JSON.parse(r.body).data !== undefined; }
-        catch(e) { return false; }
-        },
-        'no 500 server error': (r) => r.status !== 500,
-        'no 403 forbidden':    (r) => r.status !== 403,
+        'status is 200':            (r) => r.status === 200,
+        'no 400 validation error':  (r) => r.status !== 400,
+        'no 401 unauthorized':      (r) => r.status !== 401,
+        'no 403 forbidden':         (r) => r.status !== 403,
+        'no 500 server error':      (r) => r.status !== 500,
+        'has data':                 (r) => {
+            try { return JSON.parse(r.body).data !== undefined; }
+            catch(e) { return false; }
+        },  
     });
 
     sleep(1);
