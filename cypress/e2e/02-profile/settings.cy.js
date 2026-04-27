b09c7dcb128a71535a10bd43c836ffdede1c2ef9
Ahmed Attay <ahmedattay8@gmail.com>
2026-04-27 20:58:21 +0300

    Add additional settings tests and update selectors for theme and notifications

--- cypress/e2e/02-profile/settings.cy.js
+++ cypress/e2e/02-profile/settings.cy.js
@@ -20,13 +20,25 @@ describe("Settings Page", () => {
   it("Should make the UI in light mode when light mode is selected", () => {
     cy.contains(/Change theme/i).should("be.visible");
     cy.get(SettingsSelectors.themeLightInput).click();
-    cy.get("html").should("not.have.class", "dark");
+    cy.get(SettingsSelectors.documentRoot).should("not.have.class", "dark");
   });
 
   it("Should persist selected theme after reload", () => {
     cy.get(SettingsSelectors.themeLightInput).click();
     cy.reload();
-    cy.get("html").should("not.have.class", "dark");
+    cy.get(SettingsSelectors.documentRoot).should("not.have.class", "dark");
+  });
+
+  it("Should switch to dark mode when dark mode is selected", () => {
+    cy.get(SettingsSelectors.themeDarkInput).click();
+    cy.get(SettingsSelectors.documentRoot).should("have.class", "dark");
+  });
+
+  it("Should keep one theme option selected", () => {
+    cy.get(SettingsSelectors.themeAutomaticInput).click();
+    cy.get(SettingsSelectors.themeAutomaticInput).should("be.checked");
+    cy.get(SettingsSelectors.themeLightInput).should("not.be.checked");
+    cy.get(SettingsSelectors.themeDarkInput).should("not.be.checked");
   });
 
   it("Should add new email ", () => {
@@ -76,6 +88,13 @@ describe("Settings Page", () => {
     cy.contains(/Verification request submitted!/i).should("be.visible");
   });
 
+  it("Should close toast when toast close button is clicked", () => {
+    cy.get(SettingsSelectors.requestVerificationButton).click();
+    cy.contains(/Verification request submitted!/i).should("be.visible");
+    cy.get(SettingsSelectors.toastCloseButton).click();
+    cy.contains(/Verification request submitted!/i).should("not.exist");
+  });
+
   it("Should revoke all apps when user click on revoke all apps button", () => {
     cy.get(SettingsSelectors.revokeRythmifyComButton).should("be.visible");
     cy.get(SettingsSelectors.revokeAllAppsButton).click();
@@ -101,5 +120,62 @@ describe("Settings Page", () => {
     );
     cy.get(SettingsSelectors.deleteAccountCancelButton).click();
     cy.get(SettingsSelectors.deleteAccountButton).should("be.visible");
+  });
+
+  it("Should allow filling other delete reason fields in modal", () => {
+    cy.get(SettingsSelectors.deleteAccountButton).click();
+    cy.get(SettingsSelectors.deleteReasonOtherInput).click();
+    cy.get(SettingsSelectors.deleteOtherReasonInput)
+      .should("be.visible")
+      .type("Prefer another platform for now");
+    cy.get(SettingsSelectors.deleteOtherReasonInput).should(
+      "have.value",
+      "Prefer another platform for now",
+    );
+    cy.get(SettingsSelectors.deleteAccountCancelButton).click();
+  });
+
+  it("Should render social connect buttons", () => {
+    cy.get(SettingsSelectors.connectFacebookButton).should("be.visible");
+    cy.get(SettingsSelectors.connectGoogleButton).should("be.visible");
+    cy.get(SettingsSelectors.connectAppleButton).should("be.visible");
+  });
+
+  it("Should display content settings controls", () => {
+    cy.visit("/settings/content");
+    cy.get(SettingsSelectors.contentRssFeedInput).should("be.visible");
+    cy.get(SettingsSelectors.contentRssTitleInput).should("be.visible");
+    cy.get(SettingsSelectors.contentRssCategorySelect).should("be.visible");
+    cy.get(SettingsSelectors.contentRssShowEmailSelect).should("be.visible");
+    cy.get(SettingsSelectors.contentRssLanguageSelect).should("be.visible");
+    cy.get(SettingsSelectors.contentStatsServiceUrlInput).should("be.visible");
+    cy.get(SettingsSelectors.contentAuthorNameInput).should("be.visible");
+    cy.get(SettingsSelectors.contentSubscriberRedirectInput).should(
+      "be.visible",
+    );
+    cy.get(SettingsSelectors.contentCancelButton).should("be.visible");
+    cy.get(SettingsSelectors.contentSaveButton).should("be.visible");
+  });
+
+  it("Should reset edited RSS title when content cancel is clicked", () => {
+    cy.visit("/settings/content");
+    cy.get(SettingsSelectors.contentRssTitleInput)
+      .clear()
+      .type("Temporary RSS Title");
+    cy.get(SettingsSelectors.contentCancelButton).click();
+    cy.get(SettingsSelectors.contentRssTitleInput).should(
+      "not.have.value",
+      "Temporary RSS Title",
+    );
+  });
+
+  it("Should display notifications settings controls", () => {
+    cy.visit("/settings/notifications");
+    cy.get(SettingsSelectors.notificationsNewMessageDeviceSelect).should(
+      "be.visible",
+    );
+    cy.get(SettingsSelectors.notificationsCancelButton).should("be.visible");
+    cy.get(SettingsSelectors.notificationsSaveButton).should("be.visible");
+    cy.get(SettingsSelectors.notificationsSaveButton).should("be.disabled");
   });
 });
